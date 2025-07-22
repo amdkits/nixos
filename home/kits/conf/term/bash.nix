c68f749c41be6d4796774636617784cf3dc96a85
amdkits <kits.amd@gmail.com>
2025-07-23 04:49:17 +0530

    bashrc added

---
+++ home/kits/conf/term/bash.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, ... }:
+
+{
+  programs.bash = {
+    enable = true;
+
+    shellAliases = {
+      ll = "ls -lah";
+      gs = "git status";
+      ".." = "cd ..";
+      c = "clear";
+      q = "exit";
+      stfu= "shutdown now";
+      la = "ls -la";
+      n = "nvim";
+      rebuild = "sudo nixos-rebuild switch --flake ~/.dotfiles.#kits";
+    };
+
+    bashrcExtra = ''
+      export EDITOR=nvim
+      export PATH="$HOME/.local/bin:$PATH"
+      alias please='sudo'
+    '';
+
+    profileExtra = ''
+      echo "Welcome back, $USER!"
+    '';
+  };
+}
