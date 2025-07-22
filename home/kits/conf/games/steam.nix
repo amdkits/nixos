de11eb79b4f8a6d0b8743044e03e5193efd8c6fb
amdkits <kits.amd@gmail.com>
2025-07-23 03:45:47 +0530

    Steam.nix fixed

--- home/kits/conf/games/steam.nix
+++ home/kits/conf/games/steam.nix
@@ -1,5 +1,8 @@
 { config, pkgs, ... }:
 
 {
-  programs.steam.enable = true;
+  # You can define game-related apps here that Home Manager supports
+  home.packages = with pkgs; [
+    # Add optional tools/games here
+  ];
 }
